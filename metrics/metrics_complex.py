b79be3a37b37cdc909825b5a3bb7dedfdc6fe549
ringdinglinn <lalaponii@gmail.com>
2026-03-01 16:51:37 +0100

    fixed graph name bug in complex

--- metrics/metrics_complex.py
+++ metrics/metrics_complex.py
@@ -1,14 +1,11 @@
 import networkx as nx
 import metrics.network_partition as network_partition
 
-def calculate_metrics(G, graph_name):
+def calculate_metrics(G):
     metrics = {}
     metrics['transitivity']         = nx.transitivity(G)
-    print(f"{graph_name}, transitivity: {metrics['transitivity']}")
     metrics['average_clustering']   = nx.average_clustering(G)
-    print(f"{graph_name}, average_clustering: {metrics['average_clustering']}")
     metrics['cheeger constant'], _  = network_partition.compute(G)
-    print(f"{graph_name}, cheeger: {metrics['cheeger constant']}")
 
     return metrics
 
