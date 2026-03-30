5819afe985f983179730af048378083f53ea2895
ringdinglinn <lalaponii@gmail.com>
2026-03-30 23:29:14 +0200

    added interface to network partition

--- metrics/network_partition.py
+++ metrics/network_partition.py
@@ -195,12 +195,11 @@ class TorchJSONEncoder(json.JSONEncoder):
 
 # --- main --------------------------------------------------------------------
 
-def compute(G):
-    r_s = [0.05, 0.15, 0.25, 0.35, 0.45]
+def calcualte_cheeger_constant(G, r_s, passes):
     results_r = {}
     for r in r_s:
         start = time.time()
-        res = run_passes(G, r, 10)
+        res = calcualte_cheeger_constant(G, r_s, passes)
         elapsed = time.time() - start
         if res is None:
             results_r[str(r)] = None
@@ -210,3 +209,8 @@ def compute(G):
         print(f"r={r}: {elapsed:.2f}s")
 
     return min([r["cheeger"] for r in results_r.values()])
+
+def compute(G):
+    r_s = [0.05, 0.15, 0.25, 0.35, 0.45]
+    passes = 10
+    return calcualte_cheeger_constant(G, r_s, passes)
